b0273b6f3fb9d436f188447d31808f1836cd20cf
ozen-hub <info.ozenhub@gmail.com>
2024-05-27 21:28:20 +0530

    1

---
+++ src/main/java/com/devstack/ecom/upscale/repo/CustomerRepo.java
@@ -0,0 +1,8 @@
+package com.devstack.ecom.upscale.repo;
+
+import com.devstack.ecom.upscale.entity.Customer;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CustomerRepo extends JpaRepository<Customer, String> {
+
+}
---
+++ src/main/java/com/devstack/ecom/upscale/service/CustomerService.java
@@ -0,0 +1,7 @@
+package com.devstack.ecom.upscale.service;
+
+import com.devstack.ecom.upscale.dto.request.RequestCustomerDto;
+
+public interface CustomerService {
+    public void create(RequestCustomerDto dto);
+}
